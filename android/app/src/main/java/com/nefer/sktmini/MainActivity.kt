7b59b0b983db94b71705fa2126286ba205e41e7a
nefer1453 <selcukkashna@gmail.com>
2026-02-20 16:28:07 +0300

    Update MainActivity.kt

--- android/app/src/main/java/com/nefer/sktmini/MainActivity.kt
+++ android/app/src/main/java/com/nefer/sktmini/MainActivity.kt
@@ -1,25 +1,60 @@
+// kodu kopyala
 package com.nefer.sktmini
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import android.webkit.WebSettings
-import android.webkit.WebView
-import android.webkit.WebViewClient
+import android.webkit.*
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var web: WebView
+    private val HOME_URL = "https://nefer1453.github.io/skt/"
+
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        val webView = WebView(this)
-        setContentView(webView)
+        web = findViewById(R.id.webview)
 
-        webView.webViewClient = WebViewClient()
+        val s = web.settings
+        s.javaScriptEnabled = true
+        s.domStorageEnabled = true
+        s.loadsImagesAutomatically = true
 
-        val settings: WebSettings = webView.settings
-        settings.javaScriptEnabled = true
-        settings.domStorageEnabled = true
+        // Cache kaynaklı ERR_CACHE_MISS sorununu kes
+        s.cacheMode = WebSettings.LOAD_NO_CACHE
+        web.clearCache(true)
+        web.clearHistory()
 
-        // SENİN SKT sayfan:
-        webView.loadUrl("https://nefer1453.github.io/skt/")
+        // Çerez / session tutarlılığı (bazı cihazlarda lazım olabiliyor)
+        CookieManager.getInstance().setAcceptCookie(true)
+        CookieManager.getInstance().setAcceptThirdPartyCookies(web, true)
+
+        web.webChromeClient = WebChromeClient()
+        web.webViewClient = object : WebViewClient() {
+
+            override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+                return false // WebView içinde kalsın
+            }
+
+            override fun onReceivedError(
+                view: WebView,
+                request: WebResourceRequest,
+                error: WebResourceError
+            ) {
+                // Ana sayfa yüklenemediyse bir kere daha dene (ağ geç gelir vs.)
+                if (request.isForMainFrame) {
+                    view.postDelayed({ view.loadUrl(HOME_URL) }, 600)
+                }
+            }
+        }
+
+        web.loadUrl(HOME_URL)
+    }
+
+    override fun onBackPressed() {
+        if (this::web.isInitialized && web.canGoBack()) web.goBack() else super.onBackPressed()
     }
 }
